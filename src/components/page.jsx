18d7f3f164bdb2ebc6074c309b5006e26cb08e73
Freddie Heath <fheath090401@gmail.com>
2024-10-31 19:53:31 +0000

    Added SEO

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -7,6 +7,7 @@ import { useContext } from "react";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Helmet } from "react-helmet";
 
 const ProjectPage = () => {
   useEffect(() => {
@@ -26,95 +27,104 @@ const ProjectPage = () => {
   }
 
   return (
-    <section>
-      <div
-        className={`z-10 flex h-auto w-full flex-col gap-y-8 border-gray-800 px-14 py-20 text-xl xl:px-40 ${
-          darkMode ? `bg-gray-800` : `bg-off-white`
-        }`}
-      >
-        <div className="sm:pb-10">
-          <h1
-            className={`title-font pb-4 text-center text-5xl font-medium ${
-              darkMode ? `text-white` : `text-slate-800`
-            } `}
-          >
-            {project.title}
-          </h1>
-          <h1
-            className={`py-4 text-center text-sm font-medium ${
-              darkMode ? `text-white` : `text-slate-700`
-            }`}
-          >
-            {project.date}
-          </h1>
-          <h1
-            className={`py-4 text-center text-sm font-medium ${
-              darkMode ? `text-white` : `text-slate-700`
-            }`}
-          >
-            {project.tags}
-          </h1>
-        </div>
-        <div className="flex justify-center xl:pb-10">
-          <p
-            className={`font-poppins whitespace-pre-line leading-relaxed lg:px-60 ${
-              darkMode ? `text-white` : `text-slate-600`
-            }`}
-          >
-            {project.textcontent}
-          </p>
-        </div>
-        <div className="grid-col-1 grid gap-y-2 pb-10 lg:px-60">
-          <h1
-            className={`font-poppins ${
-              darkMode ? `text-white` : `text-slate-800`
-            }`}
-          >
-            Coverage:
-          </h1>
-          {project.coverage &&
-            project.coverage.map((link, index) => (
-              <a
-                key={index}
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <p
-                  className={`text-sm transition-all ${
-                    darkMode
-                      ? `text-white hover:text-gray-400`
-                      : `text-slate-700 hover:text-slate-900`
-                  }`}
-                >
-                  {link}
-                </p>
-              </a>
-            ))}
-        </div>
-        {project.video && (
-          <div data-aos="fade-right">
-            {project.video && (
-              <video controls width="100%">
-                <source src={project.video} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            )}
+    <div>
+      <Helmet>
+        <title>{project.title}</title>
+        <meta name="description" content={project.description} />
+        <meta name="author" content="Tom Davies-Pope" />
+        <meta name="robots" content="index, follow" />
+        <meta charset="UTF-8" />
+      </Helmet>
+      <section>
+        <div
+          className={`z-10 flex h-auto w-full flex-col gap-y-8 border-gray-800 px-14 py-20 text-xl xl:px-40 ${
+            darkMode ? `bg-gray-800` : `bg-off-white`
+          }`}
+        >
+          <div className="sm:pb-10">
+            <h1
+              className={`title-font pb-4 text-center text-5xl font-medium ${
+                darkMode ? `text-white` : `text-slate-800`
+              } `}
+            >
+              {project.title}
+            </h1>
+            <h1
+              className={`py-4 text-center text-sm font-medium ${
+                darkMode ? `text-white` : `text-slate-700`
+              }`}
+            >
+              {project.date}
+            </h1>
+            <h1
+              className={`py-4 text-center text-sm font-medium ${
+                darkMode ? `text-white` : `text-slate-700`
+              }`}
+            >
+              {project.tags}
+            </h1>
           </div>
-        )}
-        <div className="flex flex-wrap justify-center gap-x-8 gap-y-8 align-middle">
-          {project.images.map((image, index) => (
-            <div key={index}>
-              <img
-                alt={`Project Image ${index + 1}`}
-                src={image}
-                className="w-screen md:h-96 md:w-auto"
-              />
+          <div className="flex justify-center xl:pb-10">
+            <p
+              className={`font-poppins whitespace-pre-line leading-relaxed lg:px-60 ${
+                darkMode ? `text-white` : `text-slate-600`
+              }`}
+            >
+              {project.textcontent}
+            </p>
+          </div>
+          <div className="grid-col-1 grid gap-y-2 pb-10 lg:px-60">
+            <h1
+              className={`font-poppins ${
+                darkMode ? `text-white` : `text-slate-800`
+              }`}
+            >
+              Coverage:
+            </h1>
+            {project.coverage &&
+              project.coverage.map((link, index) => (
+                <a
+                  key={index}
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p
+                    className={`text-sm transition-all ${
+                      darkMode
+                        ? `text-white hover:text-gray-400`
+                        : `text-slate-700 hover:text-slate-900`
+                    }`}
+                  >
+                    {link}
+                  </p>
+                </a>
+              ))}
+          </div>
+          {project.video && (
+            <div data-aos="fade-right">
+              {project.video && (
+                <video controls width="100%">
+                  <source src={project.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
             </div>
-          ))}
+          )}
+          <div className="flex flex-wrap justify-center gap-x-8 gap-y-8 align-middle">
+            {project.images.map((image, index) => (
+              <div key={index}>
+                <img
+                  alt={`Project Image ${index + 1}`}
+                  src={image}
+                  className="w-screen md:h-96 md:w-auto"
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { projects } from "../data";
@@ -15,63 +16,72 @@ export default function Projects() {
     });
   }, []);
   return (
-    <section
-      id="projects"
-      className={`body-font text-gray-400 ${
-        darkMode ? "bg-gray-800" : "bg-off-white"
-      }`}
-    >
-      <div className="container mx-auto pb-4 text-center sm:px-4 sm:pb-32">
-        <div className="flex flex-wrap sm:gap-y-24">
-          {projects.map((project) => (
-            <div
-              data-aos="fade-right"
-              data-aos-once="true"
-              key={project.image}
-              className="w-full px-4 py-2 sm:py-4 lg:w-3/6"
-            >
-              <Link to={`/project/${project.id}`}>
-                <div className="relative h-72 shadow-lg sm:flex">
-                  <img
-                    alt="Project"
-                    className="absolute h-72 w-full rounded-lg object-cover object-center sm:h-96 md:inset-0"
-                    src={project.image}
-                    loading="lazy"
-                  />
+    <div>
+      <Helmet>
+        <title>Tom Davies-Pope</title>
+        <meta name="description" content="A list of my projects" />
+        <meta name="author" content="Tom Davies-Pope" />
+        <meta name="robots" content="index, follow" />
+        <meta charset="UTF-8" />
+      </Helmet>
+      <section
+        id="projects"
+        className={`body-font text-gray-400 ${
+          darkMode ? "bg-gray-800" : "bg-off-white"
+        }`}
+      >
+        <div className="container mx-auto pb-4 text-center sm:px-4 sm:pb-32">
+          <div className="flex flex-wrap sm:gap-y-24">
+            {projects.map((project) => (
+              <div
+                data-aos="fade-right"
+                data-aos-once="true"
+                key={project.image}
+                className="w-full px-4 py-2 sm:py-4 lg:w-3/6"
+              >
+                <Link to={`/project/${project.id}`}>
+                  <div className="relative h-72 shadow-lg sm:flex">
+                    <img
+                      alt="Project"
+                      className="absolute h-72 w-full rounded-lg object-cover object-center sm:h-96 md:inset-0"
+                      src={project.image}
+                      loading="lazy"
+                    />
 
-                  <div
-                    className={`sm:bg-offwhite absolute flex w-full flex-col justify-center rounded-t-lg opacity-100 transition-all sm:relative sm:h-96 sm:rounded-lg sm:border-4 sm:px-0 sm:opacity-0 xl:hover:opacity-90 ${
-                      darkMode
-                        ? `bg-slate-700 text-slate-200 sm:border-gray-500`
-                        : `bg-slate-200 text-slate-800 sm:border-slate-600`
-                    }`}
-                  >
-                    <motion.div
-                      className="flex h-full w-full items-center rounded-t-lg"
-                      whileHover={{ scale: 1.05 }}
+                    <div
+                      className={`sm:bg-offwhite absolute flex w-full flex-col justify-center rounded-t-lg opacity-100 transition-all sm:relative sm:h-96 sm:rounded-lg sm:border-4 sm:px-0 sm:opacity-0 xl:hover:opacity-90 ${
+                        darkMode
+                          ? `bg-slate-700 text-slate-200 sm:border-gray-500`
+                          : `bg-slate-200 text-slate-800 sm:border-slate-600`
+                      }`}
                     >
-                      <div className="flex flex-col justify-center rounded-t-lg px-8">
-                        <h1 className="title-font hidden py-3 text-lg font-medium sm:block sm:pt-0 sm:font-bold">
-                          {project.title}
-                        </h1>
-                        <p className="hidden py-5 font-medium leading-relaxed sm:block">
-                          {project.description}
-                        </p>
-                        <p className="hidden pb-5 text-sm font-medium sm:block">
-                          {project.tags}
-                        </p>
-                        <p className="hidden text-xs font-medium sm:block">
-                          {project.date}
-                        </p>
-                      </div>
-                    </motion.div>
+                      <motion.div
+                        className="flex h-full w-full items-center rounded-t-lg"
+                        whileHover={{ scale: 1.05 }}
+                      >
+                        <div className="flex flex-col justify-center rounded-t-lg px-8">
+                          <h1 className="title-font hidden py-3 text-lg font-medium sm:block sm:pt-0 sm:font-bold">
+                            {project.title}
+                          </h1>
+                          <p className="hidden py-5 font-medium leading-relaxed sm:block">
+                            {project.description}
+                          </p>
+                          <p className="hidden pb-5 text-sm font-medium sm:block">
+                            {project.tags}
+                          </p>
+                          <p className="hidden text-xs font-medium sm:block">
+                            {project.date}
+                          </p>
+                        </div>
+                      </motion.div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          ))}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
